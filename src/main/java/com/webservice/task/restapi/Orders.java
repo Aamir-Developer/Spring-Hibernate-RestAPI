3ab2f4068cb4f35090c1ca8ee91327d0919663b7
Aamir Mohammed <maamir@mail.uni-paderborn.de>
2021-03-05 16:36:15 +0100

    orders by id is retrieving all details of the order

--- src/main/java/com/webservice/task/restapi/Orders.java
+++ src/main/java/com/webservice/task/restapi/Orders.java
@@ -15,14 +15,59 @@ public class Orders {
 	private Integer orderNumber;
 	private String status;
 	private Date orderDate;
+	private Date requiredDate;
+	private Date shippedDate;
+	private String comments;
+	private Integer customerNumber;
 
 	public Orders() {
 	}
 
-	public Orders(Integer orderNumber, String status, Date orderDate) {
+	public Orders(Integer orderNumber, String status, Date orderDate, Date requiredDate, Date shippedDate,
+			String comments, Integer customerNumber) {
 		this.orderNumber = orderNumber;
 		this.status = status;
 		this.orderDate = orderDate;
+		this.requiredDate = requiredDate;
+		this.shippedDate = shippedDate;
+		this.comments = comments;
+		this.customerNumber = customerNumber;
+	}
+
+	@Column(name = "requiredDate")
+	public Date getRequiredDate() {
+		return requiredDate;
+	}
+
+	public void setRequiredDate(Date requiredDate) {
+		this.requiredDate = requiredDate;
+	}
+
+	@Column(name = "shippedDate")
+	public Date getShippedDate() {
+		return shippedDate;
+	}
+
+	public void setShippedDate(Date shippedDate) {
+		this.shippedDate = shippedDate;
+	}
+
+	@Column(name = "comments")
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	@Column(name = "customerNumber")
+	public Integer getCustomerNumber() {
+		return customerNumber;
+	}
+
+	public void setCustomerNumber(Integer customerNumber) {
+		this.customerNumber = customerNumber;
 	}
 
 	@Id
